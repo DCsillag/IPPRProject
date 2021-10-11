3899aa6f912bf1df211f1d94c8e3fd7aa38b4a05
Daniel Csillag <danielcsillag@hotmail.com>
2021-10-11 19:04:44 +1100

    Built a very basic edge detector

--- Test.py
+++ Test.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
 
-print("Hello World")
-print("Second Change")
+import cv2 as cv
+
+img = cv.imread("dataset/train/142.jpg")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+edge = cv.Canny(gray, 5,5)
+
+cv.imshow("142", img)
+cv.imshow("142_gray", gray)
+cv.imshow("Canny", edge)
+
+cv.waitKey(0)
