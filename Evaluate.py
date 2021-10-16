f899809fe81d7b97cd07e84b82e1d77cebeb61be
Daniel Csillag <danielcsillag@hotmail.com>
2021-10-16 20:35:01 +1100

    Add a program outline to Evaluation.py

---
+++ Evaluate.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import cv2 as cv
+from LoadShowBoundingBox import *
+
+# Setting out a general structure to this document
+
+# Define a function that when given an image number, and a predicted coordinate location, return an IoU value
+def calculateOverlap(img_num, pred_cords):
+    return None
+
+# Define a function that evalutes the category of outcome.
+# If IoU > Threshold : True Positive, 
+# If IoU < Threshold : False Positive
+# If IoU 0 Then False Negative
+def calculateClassfication():
+    return None
+
+# Define a function that when given two sets of coordinates, returns an IoU score.
+def calculateIoU():
+    return None
+
+
+
+
+
+
--- EvaluteIntersection.py
+++
@@ -1,5 +0,0 @@
-import os
-import numpy as np
-import cv2 as cv
-from LoadShowBoundingBox import *
-
