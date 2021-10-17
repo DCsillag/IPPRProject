6395f021b6f71f15c709e014d314b6ca9346e7da
Daniel Csillag <danielcsillag@hotmail.com>
2021-10-17 19:44:15 +1100

    Written Function to Evaluate Raw IOU score for 2 bounding boxes

--- Evaluate.py
+++ Evaluate.py
@@ -13,15 +13,29 @@ def calculateOverlap(img_num, pred_cords):
 # If IoU > Threshold : True Positive, 
 # If IoU < Threshold : False Positive
 # If IoU 0 Then False Negative
-def calculateClassfication():
+def calculateClassfication(iou, thresh):
     return None
 
 # Define a function that when given two sets of coordinates, returns an IoU score.
-def calculateIoU():
-    return None
+def calculateIoU(boxA, boxB):
+    # Find the dimensions of the internal rectangle.
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # Compute the area of the central rectangle
+    innerArea = max(0, xB - xA + 1) * max(0, yB - yA +1)
+
+    # Compute area of each box
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # Compute and return intersection of union
+    return innerArea / float(boxAArea + boxBArea - innerArea)
 
 
-
-
+# Test
+# print(calculateIoU(getPlateCords(0), [1200, 2000, 2000, 2300]))
 
 
