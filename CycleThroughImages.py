f3b20e3d2c4d565154b7c47f98b4b7648ba1d40f
lvsarose <62104698+lvsarose@users.noreply.github.com>
2021-10-21 17:00:48 +1100

    experiment

--- CycleThroughImages.py
+++ CycleThroughImages.py
@@ -6,7 +6,11 @@ DIR = r"\\dataset\\"
 # DIR = './dataset/'
 
 # Update two values based on image ranges
+<<<<<<< Updated upstream
 for i in range(400,410):
+=======
+for i in range(0,100):
+>>>>>>> Stashed changes
     cv.imshow(str(i)+" IMG", cv.imread(DIR+str(i)+".jpg"))
     cv.waitKey(0)
 
