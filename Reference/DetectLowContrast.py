bb8110f18e13ee0478f24caf66e2ad211e91d669
fellyli <feliciali28137@gmail.com>
2021-10-25 17:30:40 +1100

    Updated Detectlowcontrast

--- Reference/DetectLowContrast.py
+++ Reference/DetectLowContrast.py
@@ -6,7 +6,7 @@ from skimage.exposure import is_low_contrast
 #used to examine image's histogram and then determining if the range of brightness spans less than a fractional amount
 
 
-#construct argument parser
+#construct argument parser [NEW]
 ap = ag.ArgumentParser()
 ap.add_argument("-t", "--thresh", type=float, default = 0.30, help = "threshold for low contrast")
 #image to be considred low cast if it is less than 30% of the range of brightness
@@ -17,14 +17,14 @@ img = cv.imread('dataset/50.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 #blur and edge image
-blurred = cv.GaussianBlur(gray, (5, 5), 0)
+blurred = cv.GaussianBlur(gray, (5, 5), 0)   #[NEW]]
 edged = cv.Canny(blurred, 30, 200)
 
-#intialise the text and color of input image, assuming that image is not low contrast
+#intialise the text and color of input image, assuming that image is not low contrast [NEW]
 text = "Not low contrast image"
 color = (0, 255, 0)
 
-#detects if low contrast
+#detects if low contrast [NEW]
 if is_low_contrast (gray, fraction_threshold=args["thresh"]):
     #update the text and color if so
     text = "Low contrast image"
@@ -47,7 +47,7 @@ mask = np.zeros(gray.shape, np.uint8)  #created blank mask (same shape as og gra
 lp = cv.drawContours(mask,[location],0,255,-1,) #draw contours in the image, want to draw location
 lp = cv.bitwise_and(img,img,mask=mask) #overlap mask, return segment of our iamge ie numberplate
 
-#draw text on output image
+#draw text on output image [NEW]
 cv.putText(lp, text, (5,25), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.8, color, 2)
 
 #show image
